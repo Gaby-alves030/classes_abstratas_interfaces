92a90488c8aab53043c417b82252cbf1782f1e31
Gaby <gaabyrocha574@gmail.com>
2021-11-18 19:57:24 +0000

    Conta

---
+++ Teste.java
@@ -0,0 +1,12 @@
+public class Teste {
+    
+    public static void main(String[] args) {
+
+        ContaEspecializada contaEspecializada=new ContaEspecializada();
+//Contajavac
+        contaEspecializada.setSaldo(25000);
+
+        contaEspecializada.imprimeExtrato();
+
+    }
+}
